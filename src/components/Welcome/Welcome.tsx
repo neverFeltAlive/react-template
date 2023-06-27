aac5e2a87f86f3e601945612171c10d6fc9740a4
sofaprog <rsh@hawkingbros.com>
2023-06-27 16:40:57 +0300

    Initial Commit

---
+++ src/components/Welcome/Welcome.tsx
@@ -0,0 +1,5 @@
+import { WelcomeHeading } from './Welcome.styled';
+
+export const Welcome = () => {
+  return <h1>React Template</h1>;
+};
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build'),
+    },
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@pages': path.resolve(__dirname, './src/pages/index.ts'),
+      '@components': path.resolve(__dirname, './src/components/index.ts'),
+    },
+  },
+};
